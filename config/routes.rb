88a50cb97b74827b2db8ba685a1eeebceb92dcc6
Rogier <rogier@blijblijblij.com>
2020-06-24 16:49:56 +0200

    Add default /

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'index#index'
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
