8efee554656a4c66318256161ca5ebfae38773e4
Rogier <rogier@blijblijblij.com>
2020-06-24 16:51:01 +0200

    Add first index page

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SchooldehofakkerNl
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # errors are now generated dynamically
+    config.exceptions_app = self.routes
   end
 end
