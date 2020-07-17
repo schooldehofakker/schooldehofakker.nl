9c0afddbf77a04825eb1faa50f8efa578424db69
Rogier <rogier@blijblijblij.com>
2020-07-17 13:52:21 +0200

    Add kracht van de hofakker

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'index#index'
 
+  get 'kracht-van-de-hofakker', to: 'kracht#index'
+
   match '/404', to: 'errors#not_found', via: :all
   match '/422', to: 'errors#unacceptable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
