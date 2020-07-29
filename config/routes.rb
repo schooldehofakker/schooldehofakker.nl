23b63dcd15f8f3a8a5bf3b2c332b9206f8ba1acf
Rogier <rogier@blijblijblij.com>
2020-07-29 21:18:28 +0200

    Fix contact route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'index#index'
 
+  get 'contact', to: 'contact#index'
   get 'kracht-van-de-hofakker', to: 'kracht#index'
   get 'leraren', to: 'leraren#index'
   resources 'nieuws'
