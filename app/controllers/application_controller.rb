22d52e98b38246cc870631b62bc6ff1f6446b09e
Rogier <rogier@blijblijblij.com>
2020-08-04 11:32:22 +0200

    Add Null object for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= super || Guest.new
+  end
+
+  def user_signed_in?
+    current_user.is_a? User
+  end
+end
+
+# Create null object for Guest user
+class Guest
+  def admin?
+    false
+  end
 end
