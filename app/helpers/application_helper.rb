e5cefe5fbbd81de596078ea7b8252c5f2ee6e0b7
Rogier <rogier@blijblijblij.com>
2020-08-07 17:46:35 +0200

    Fix selected menu item #51

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  # helper to check whether selected page must change style to
+  # accentuate being selected
   def active_class(link_path)
     current_page?(link_path) ? 'item--current' : ''
   end
