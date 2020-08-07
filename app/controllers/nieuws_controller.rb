e875c6cd1a4b2a17193755ca618032d4b630f03c
Rogier <rogier@blijblijblij.com>
2020-08-07 17:41:24 +0200

    Fix some translations

--- app/controllers/nieuws_controller.rb
+++ app/controllers/nieuws_controller.rb
@@ -21,7 +21,7 @@ class NieuwsController < ApplicationController
     @news = News.new(post_params)
 
     if @news.save
-      redirect_to nieuw_path(@news), notice: 'News was successfully created.'
+      redirect_to nieuw_path(@news), notice: 'Nieuws is bijgewerkt.'
     else
       render :new
     end
@@ -32,7 +32,7 @@ class NieuwsController < ApplicationController
   def update
     pd = Date.new(params[:news]['published_at(1i)'].to_i, params[:news]['published_at(2i)'].to_i, params[:news]['published_at(3i)'].to_i)
     if @news.update(title: post_params[:title], content: post_params[:content], status: post_params[:status], published_at: pd)
-      redirect_to nieuw_path(@news), notice: 'News was successfully updated.'
+      redirect_to nieuw_path(@news), notice: 'Nieuws is bijgewerkt.'
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class NieuwsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @news.destroy
-    redirect_to nieuws_url, notice: 'News was successfully destroyed.'
+    redirect_to nieuws_url, notice: 'Nieuws is verwijderd.'
   end
 
   private
@@ -52,21 +52,13 @@ class NieuwsController < ApplicationController
     redirect_to nieuws_path unless current_user.admin?
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_news
-    @news = News.friendly.find(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:news).permit(:title, :content, :status, :published_at)
   end
 
-  def flatten_date_array(hash)
-    puts '#' * 60
-    puts 'app/controllers/nieuws_controller.rb:63'
-    puts '*' * 60
-    puts hash.inspect
-    %w[1 2 3].map { |e| hash["date(#{e}i)"].to_i }
+  # Use callbacks to share common setup or constraints between actions.
+  def set_news
+    @news = News.friendly.find(params[:id])
   end
 end
