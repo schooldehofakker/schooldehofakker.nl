a71ebed9f902d65e4a615982beff182c31c50c76
Rogier <rwessel@reelmetrics.com>
2020-08-27 13:33:31 +0200

    Seed admin (which uses bootstrap password)

---
+++ db/seeds/administrator.rb
@@ -0,0 +1,32 @@
+module Seeds
+  module Administrator
+    def self.included(_modul)
+      _modul.do_seed
+    end
+
+    class << self
+      attr_accessor :user
+    end
+
+    class_eval do
+      def do_seed
+        Seeds::Administrator.user = seed_user
+      end
+
+      def seed_user
+        user = User.find_or_create_by!(email: p('blijblijblij@hey.com')) do |newuser|
+          newuser.first_name = 'Rogier'
+          newuser.last_name = 'Wessel'
+          newuser.admin = true
+          newuser.password = Rails.application.credentials.bootstrap[:password]
+          newuser.password_confirmation = Rails.application.credentials.bootstrap[:password]
+        end
+        user.update! first_name: 'Rogier', last_name: 'Wessel', admin: true
+        user.password = Rails.application.credentials.bootstrap[:password]
+        user.password_confirmation = Rails.application.credentials.bootstrap[:password]
+        user.save!
+        user
+      end
+    end
+  end
+end
