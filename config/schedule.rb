f6ccecdb4260289c719ab42bcf851ca1dabdfeee
Rogier <rogier@blijblijblij.com>
2020-09-04 14:57:08 +0200

    Only run whenever on production

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,8 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 1.day, at: '5:00 am' do
-  rake '-s sitemap:refresh'
+if @environment == 'production'
+  every 1.day, at: '5:00 am' do
+    rake '-s sitemap:refresh'
+  end
 end
