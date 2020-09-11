7658e124c739c3a8e4dc995b4ba55c7fa079bd8a
Rogier <rogier@blijblijblij.com>
2020-09-11 12:54:20 +0200

    Cache news

--- app/controllers/nieuws_controller.rb
+++ app/controllers/nieuws_controller.rb
@@ -1,6 +1,10 @@
 # index page
 class NieuwsController < ApplicationController
-  def index; end
+  def index
+    @allnews = cache_data(key: 'nieuws', time: 1.hour) do
+      @content.entries(content_type: 'nieuws', order: '-fields.published')
+    end
+  end
 
   def show
     @news = @content.entries(content_type: 'nieuws', 'fields.slug[in]' => params[:id]).first
