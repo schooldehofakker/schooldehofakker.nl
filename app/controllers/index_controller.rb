718dcf38c77eab58eaa0d741a861f5fa5eec039e
Rogier <rogier@blijblijblij.com>
2020-09-11 12:27:27 +0200

    Cache index calls to api

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,8 @@
 # index page
 class IndexController < ApplicationController
   def index
-    @index = @content.entries(content_type: 'pagina', 'fields.slug[in]' => 'over-de-school').first
+    @index = cache_data(key: 'over-de-school') do
+      @content.entries(content_type: 'pagina', 'fields.slug[in]' => 'over-de-school').first
+    end
   end
 end
