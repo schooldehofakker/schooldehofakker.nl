4669529d1cf7305f2f486c5d15378256192bb174
Rogier <rogier@blijblijblij.com>
2020-09-11 12:54:01 +0200

    Cache vacatures

--- app/controllers/vacatures_controller.rb
+++ app/controllers/vacatures_controller.rb
@@ -1,4 +1,8 @@
 # index page
 class VacaturesController < ApplicationController
-  def index; end
+  def index
+    @vacatures = cache_data(key: 'vacatures', time: 1.hour) do
+      @content.entries(content_type: 'vacatures', include: 2)
+    end
+  end
 end
