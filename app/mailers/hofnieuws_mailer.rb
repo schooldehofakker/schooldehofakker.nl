5d56ef2136110cefe1b694d3a864cb2c6fe26072
Rogier <rogier@blijblijblij.com>
2021-01-24 22:53:12 +0100

    Add text version, remove pdf

--- app/mailers/hofnieuws_mailer.rb
+++ app/mailers/hofnieuws_mailer.rb
@@ -1,7 +1,7 @@
 class HofnieuwsMailer < ApplicationMailer
   def send_hofnieuws_email(user)
     @user = user
-    attachments['Hofnieuws-lang-1-2020.pdf'] = File.read('app/assets/pdf/Hofnieuws-lang-1-2020.pdf')
+    # attachments['Hofnieuws-lang-1-2020.pdf'] = File.read('app/assets/pdf/Hofnieuws-lang-1-2020.pdf')
     mail({
            to: user.email,
            from: 'mailing@schooldehofakker.nl',
