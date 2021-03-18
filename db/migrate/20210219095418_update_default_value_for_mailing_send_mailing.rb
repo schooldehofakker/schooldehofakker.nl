517a2536b51c2f9716b37b69b1eeea28f792fdc9
Rogier Wessel <rogier@blijblijblij.com>
2021-03-18 11:47:08 +0100

    Update 20210219095418_update_default_value_for_mailing_send_mailing.rb

--- db/migrate/20210219095418_update_default_value_for_mailing_send_mailing.rb
+++ db/migrate/20210219095418_update_default_value_for_mailing_send_mailing.rb
@@ -2,7 +2,7 @@ class UpdateDefaultValueForMailingSendMailing < ActiveRecord::Migration[6.1]
   def change
   execute <<-SQL
   UPDATE mailings
-  SET mailing_send = false
+  SET mailing_send = true
   where mailing_send is NULL;
   SQL
   end
